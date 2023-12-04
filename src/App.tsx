36606b3871cdba03a7b0754fad15f865de29ca55
Mohammad Mahdi Mohammadi <phoneticem@gmail.com>
2023-12-04 23:41:38 +0330

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import {useState} from "react";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      adasda
+    </>
+  )
+}
+
+export default App
